78fd01aa75035bd7336986a01f93a82bfb12eff3
jackcasey067 <jackcasey067@gmail.com>
2022-07-03 16:01:11 -0500

    Deploy 2.0

--- start.py
+++ start.py
@@ -3,10 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-sockets = Sockets(app)
-redis = redis.from_url(REDIS_URL)
-
-
 @app.route('/')
 def hello():
-    app.send_static_file('index.html')
+    return app.send_static_file('index.html')
